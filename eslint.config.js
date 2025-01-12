3a6d2b064e7990168bdcfc8f20b50d4ee464b623
gui117 <48198890+gui117@users.noreply.github.com>
2025-01-12 16:21:22 +0800

    feat: integrate UnoCSS and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,6 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   unocss: true,
+  astro: true,
   formatters: true,
 })
---
+++ uno.config.ts
@@ -0,0 +1,37 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [],
+  rules: [],
+  safelist: [],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'text-bottom',
+      },
+    }),
+    presetTypography(),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+  content: {
+    pipeline: {
+      include: [
+        // the default
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // include js/ts files
+        '(components|src)/**/*.{js,ts}',
+      ],
+    },
+  },
+})
