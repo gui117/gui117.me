9849b00f972a0e0156dfe3c7d0aeca09e984f9ae
gui117 <48198890+gui117@users.noreply.github.com>
2025-01-14 16:43:39 +0800

    feat: add header component, update global styles.

--- uno.config.ts
+++ uno.config.ts
@@ -9,7 +9,20 @@ import {
 } from 'unocss'
 
 export default defineConfig({
-  shortcuts: [],
+  shortcuts: {
+    'border-base': 'border-gray-200 dark:border-gray-800',
+    'bg-active': 'bg-gray:20',
+    'bg-faded': 'bg-gray:5',
+    'bg-base': 'bg-white dark:bg-[#020420]',
+    'text-faded': 'text-gray6:100 dark:text-gray3:80',
+
+    'bg-code': 'bg-gray/5',
+
+    'bg-inline-code': 'bg-gray-100 dark:bg-gray-800/75',
+    'border-inline-code': 'border-gray-200 dark:border-gray-700/75',
+
+    'icon-link': 'h-9 inline-flex items-center justify-center whitespace-nowrap rounded-md p-2 text-sm font-medium ring-offset-background transition-colors disabled:pointer-events-none hover:bg-accent hover:text-accent-foreground disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-ring',
+  },
   rules: [],
   safelist: [],
   presets: [
